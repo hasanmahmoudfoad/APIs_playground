cbc1bedbd2f4b6cffbcfce388440f1008dc0db3b
unknown <hasanmahmoudfoad@gmail.com>
2023-04-23 01:18:10 -0700

    first commit

---
+++ api_test.js
@@ -0,0 +1,20 @@
+let test = new XMLHttpRequest;
+
+
+// test.open("get", "https://jsonplaceholder.typicode.com");
+
+
+test.open("get", "https://jsonplaceholder.typicode.com");
+test.send();
+
+
+
+
+
+
+
+
+
+
+
+
